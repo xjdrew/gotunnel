7eee427c1f0c18ba5ecd21e95403c72d7a648a00
xjdrew <xj.drew@gmail.com>
2014-07-16 18:29:39 +0800

    support multi-tunnels

--- tunnel/front_door.go
+++ tunnel/front_door.go
@@ -6,103 +6,48 @@
 package tunnel
 
 import (
-	"net"
 	"sync"
 )
 
+var frontServer *FrontServer
+var once sync.Once
+
 type FrontDoor struct {
-	TcpServer
-	wg   sync.WaitGroup
 	coor *Coor
-}
-
-func (self *FrontDoor) pump() {
-	defer self.wg.Done()
-	self.coor.Start()
-	self.coor.Wait()
-	self.closeListener()
-}
-
-func (self *FrontDoor) listen() {
-	defer self.wg.Done()
-	for {
-		conn, err := self.accept()
-		if err != nil {
-			Error("front server acceept failed:%s", err.Error())
-			break
-		}
-		Debug("front server, new connection from %v", conn.RemoteAddr())
-		self.wg.Add(1)
-		go self.handleClient(conn)
-	}
-}
-
-func (self *FrontDoor) Start() error {
-	err := self.buildListener()
-	if err != nil {
-		return err
-	}
-
-	self.wg.Add(1)
-	go self.listen()
-	self.wg.Add(1)
-	go self.pump()
-
-	return nil
-}
-
-func (self *FrontDoor) handleClient(conn *net.TCPConn) {
-	defer self.wg.Done()
-
-	// try skip tgw
-	err := skipTGW(conn)
-	if err != nil {
-		Error("skip tgw failed, source: %v", conn.RemoteAddr())
-		conn.Close()
-		return
-	}
-
-	linkid := self.coor.AcquireId()
-	if linkid == 0 {
-		Error("alloc linkid failed, source: %v", conn.RemoteAddr())
-		conn.Close()
-		return
-	}
-
-	ch := make(chan []byte, 256)
-	err = self.coor.Set(linkid, ch)
-	if err != nil {
-		//impossible
-		conn.Close()
-		Error("set link failed, linkid:%d, source: %v", linkid, conn.RemoteAddr())
-		return
-	}
-
-	Info("link(%d) create link, source: %v", linkid, conn.RemoteAddr())
-	defer self.coor.ReleaseId(linkid)
-
-	self.coor.SendLinkCreate(linkid)
-
-	link := NewLink(linkid, conn)
-	link.Pump(self.coor, ch)
 }
 
 func (self *FrontDoor) Reload() error {
 	return nil
 }
 
+func (self *FrontDoor) Start() error {
+	frontServer.addCoor(self.coor)
+	return self.coor.Start()
+}
+
 func (self *FrontDoor) Stop() {
-	self.closeListener()
 }
 
 func (self *FrontDoor) Wait() {
-	self.wg.Wait()
-	Error("front door quit")
+	self.coor.Wait()
+	frontServer.removeCoor(self.coor)
+}
+
+func createFrontServer() {
+	frontServer = NewFrontServer()
+	go func() {
+		err := frontServer.Start()
+		if err != nil {
+			Panic("start front server failed:%s", err.Error())
+		}
+		frontServer.Wait()
+	}()
 }
 
 func NewFrontDoor(tunnel *Tunnel) Service {
+	once.Do(createFrontServer)
+
 	frontDoor := new(FrontDoor)
 	frontDoor.coor = NewCoor(tunnel, nil)
-	frontDoor.TcpServer.addr = options.FrontAddr
 	return frontDoor
 }
